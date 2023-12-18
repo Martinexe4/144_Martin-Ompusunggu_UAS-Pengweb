0ca13bfdc8306d36748d77941a88d99a6567c41e
121140144_Martin Ompusunggu <86707999+Martinexe4@users.noreply.github.com>
2023-12-18 09:24:57 +0700

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <hr style="margin-top:40px;">
-    <p style="text-align:center;z-index:0;margin-top:10px;">© 2022 Inventory and Sales System By Chromatica</p>
+    <p style="text-align:center;z-index:0;margin-top:10px;">© 2023 Inventory and Sales System</p>
 </footer>
